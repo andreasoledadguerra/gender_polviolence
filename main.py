74d3b1e0837a0de02970bcffae9d26a3c85dbc8a
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-11-22 09:40:54 -0300

    customizing main.py with streamlit settings

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import streamlit as st
-
-
-st.write('')
-
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots(1,1)
-ax.plot([0,1.1,2,0.7])
-st.pyplot(fig)
+st.title('Political Violence across the world Data Explorer')
+st.text('This is a web app to explore political violence data')
+
+#fig, ax = plt.subplots(1,1)
+#ax.plot([0,1.1,2,0.7])
+#st.pyplot(fig)
