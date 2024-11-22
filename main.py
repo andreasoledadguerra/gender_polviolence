38ac284c26bd748cc303c0c1a91da93dafcf7b09
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-11-22 10:57:20 -0300

    Functions plot is added to main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
 
 st.title('Political Violence across the world Data Explorer')
 st.text('This is a web app to explore political violence data')
+
 
 #fig, ax = plt.subplots(1,1)
 #ax.plot([0,1.1,2,0.7])
