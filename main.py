7e85f7b8cc04a371265bf0dc1884cd3ca25cae56
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-11-24 23:34:10 -0300

    Testing plot_fatalities_per_region in main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from pol_violence.plots import plot_fatalities_per_region
 
 st.title('Political Violence across the world Data Explorer')
 st.text('This is a web app to explore political violence data')
 
+
+st.pyplot(fig)
 
 #fig, ax = plt.subplots(1,1)
 #ax.plot([0,1.1,2,0.7])
