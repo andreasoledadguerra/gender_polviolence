65ff7a8067e6dc17fe4c6bc00007f7df8b6fb910
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-11-25 11:38:24 -0300

    Functions are imported

---
+++ pol_violence/__init__.py
@@ -0,0 +1,5 @@
+from .plots import plot_fatalities_per_region
+from .plots import plot_region_event_ondemand
+from .plots import plot_all_region_event_highlight
+from .plots import plot_stacked_bar
+
