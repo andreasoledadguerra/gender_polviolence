fa306d04a897495a91c212d3439953590caa7bb0
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-12-03 11:34:56 -0300

    'plot_region_event_ondemand' selectbox and button is created

--- main.py
+++ main.py
@@ -113,25 +113,25 @@ if st.button('Get your plot'):
 
 
 # A variable for exploring 'notes' is created
-col3,col4 = st.columns(2)
+#col3,col4 = st.columns(2)
 
-with col3:
+#with col3:
     # Selectbox for choosing the variable 'region'
-    region = st.selectbox(
-    "Select region",
-    (df_gpv['region'].unique())
-)
+    #region = st.selectbox(
+    #"Select region",
+    #(df_gpv['region'].unique())
+#)
     
-with col4:
+#with col4:
     #Selectbox for choosing the variable 'event'
-    event = st. selectbox(
-        "Select event",
-        (df_gpv['sub_event_type'].unique())
-)
+    #event = st. selectbox(
+        #"Select event",
+        #(df_gpv['sub_event_type'].unique())
+#)
 
 ## A button to perform analysis is created
-if st.button('Get your number of event per region'):    
-    st.write(get_count_by_region_event(counts_dict, region, event))     
+#if st.button('Get your number of event per region'):    
+    #st.write(get_count_by_region_event(counts_dict, region, event))     
     #st.write(f'The numbers of fatalities of {option_region} is {fatalities_per_region[option_region]}')
 
 #The output is ALWAYS "The number of Attack in africa is 3036"
@@ -158,6 +158,50 @@ if st.button('Get your stacked plot'):
     st.pyplot(plot_stacked_bar(grouped_counts))
 
 
+#Plot Region-Event on Demand
+def plot_region_event_ondemand(counts_dict: dict, region: str, event: str) -> object:
+    
+    filtered_data = {(region, event): counts_dict.get((region, event), 0)}# Returns the value if found, and 0 if not found (default value)
+
+    x_labels = [f"{region} - {event}"]  # Single bar for the specified region-event
+    colors = ['mediumslateblue']  # Highlight color for the bar
+
+    fig, ax = plt.subplots(figsize=(6, 4))
+
+    bars = ax.bar(x_labels, filtered_data.values(), color=colors)
+
+    ax.set_xlabel('Event per Region', fontsize=12)
+    ax.set_ylabel('Count', fontsize=12)
+    ax.set_title('Counting Events per Region', fontsize=14)
+    ax.tick_params(axis='x', rotation=0)
+
+    plt.close()
+    return fig
+
+# Variables for plot event-per-region on demand
+col_1,col_2, = st.columns(2)
+
+with col_1:
+    #Selectbox for choosing the variable 'region'
+    region_1 = st. selectbox(
+        "Select region",
+        (df_gpv['region'].unique())
+)
+    
+with col_2:
+    # Selectbox for choosing the variable 'event'
+    event_1 = st.selectbox(
+    "Select event",
+    (df_gpv['sub_event_type'].unique())
+)    
+
+st.write("You selected:", region_1, event_1)
+
+## A button to perform analysis is created
+if st.button('Get your plot region/event colored on demand'):     
+    
+    st.write(f'This is a plot of {event_1} in {region_1}')
+    st.pyplot(plot_region_event_ondemand(counts_dict, region_1,event_1))
 
 
 
