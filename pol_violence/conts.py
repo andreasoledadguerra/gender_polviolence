cad8c9845f6d501951fd4a83926981223f95c0a0
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-12-23 11:48:12 -0300

    PATH_DATA_DF, COL_REGION, COL_FATALITES, COL_SUB_EVENT_TYPE, TITLE_APP, SUBTITLE_APP, COLOR_DARK_1 and COLOR LIGHT_1 constants are added to const.py

---
+++ pol_violence/conts.py
@@ -0,0 +1,18 @@
+# ----------- ------------------Dataframe's path --------------------------------------
+PATH_DATA_DF = 'gender_Sep27-1.csv'
+
+
+# ----------- -------------- Columns from dataframe ------------------------------------
+COL_REGION = 'region'
+COL_FATALITIES = 'fatalities'
+COL_SUB_EVENT_TYPE = 'sub_event_type'
+
+
+# ----------- -------------- Strings to display in app-----------------------------------
+TITLE_APP = 'Political Violence across the world Data Explorer'
+SUBTITLE_APP = 'This is a web app to expore political violence data'
+
+
+# -----------------------------------------Colors ----------------------------------------
+COLOR_DARK_1 = 'darkslategray'
+COLOR_LIGHT_1 = 'red'
