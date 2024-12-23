9e1fb2893f8db2471325d14ff580d4c61ac1d205
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-12-23 12:00:42 -0300

    'map_region_to_normalized_region' is added to 'data_loaded.py'

--- pol_violence/data_loaded.py
+++ pol_violence/data_loaded.py
@@ -1,10 +1,33 @@
 import pandas as pd
 from datetime import date
 from pol_violence.const import PATH_DATA_DF, COL_REGION 
+
+def map_region_to_normalized_region(df_gpv: pd.DataFrame) -> pd.DataFrame:
+    # A dictionary is created 
+    region_map = {
+        'africa': ['Southern Africa', 'Northern Africa', 'Middle Africa', 'Western Africa', 'Eastern Africa'],
+        'middle east': ['Middle East'],
+        'asia':['Caucasus and Central Asia', 'Sotheast Asia', 'South Asia', 'East Asia'],
+        'america and caribbean' :[ 'South America', 'North America', 'Central America', 'Caribbean'],
+        'europe': ['Europe'],
+        'oceania': ['Oceania']  
+    }
+
+    # Flatten the mapping to map each element to its group
+    flattened_map = {}
+    for key, values in region_map.items():
+        for item in values:
+            flattened_map[item] = key
+
+
+    #new_column = {'COL_REGION': ['america', 'middle east', 'asia', 'america and caribbean', 'europe', 'oceania']}
+    df_gpv['COL_REGION'] = df_gpv['COL_REGION'].map(flattened_map)
+    return df_gpv
+
+
 def load_dataframe() -> pd.DataFrame :
-    
     # Dataset is downloaded
-    df_gpv = pd.read_csv("gender_Sep27-1.csv")
+    df_gpv = pd.read_csv(PATH_DATA_DF)
     df_gpv.drop([
         'event_id_cnty',
         #'event_date', 
